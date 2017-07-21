e0976535a62eece9caed830f5183b0d25e22b41b
mikecrarer <mike@crarer.co.uk>
2017-07-21 07:13:10 +0100

    Now we have a map... there is no testing going on yet though.. oops

--- adventure.py
+++ adventure.py
@@ -20,14 +20,41 @@ while True:
         if possible_location:
             print 'to the %s is a %s' %(k, possible_location)
             valid_directions[k] = possible_position
+    print "\nhere's a map to help you..."
+
+    if location[0] == "h":
+        print "H   P\nL"
+    elif location[0] == "p":
+        print "H   P\n    M"
+    elif location[0] == "l":
+        print "H   \nL   M"
+    elif location[0] == "m":
+        print "    P\nL   M"
+
+    print "\n"
+
+
 
     direction = raw_input('which direction do you want to go ?\n')
 
     new_position = valid_directions.get(direction)
     if new_position:
         position = new_position
+        print new_position
+
+
     else:
         print "sorry, that is not a valid direction"
+
+
+
+
+
+
+
+
+
+
 
 
 
